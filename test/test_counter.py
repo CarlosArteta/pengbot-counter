6d2d24a355fb736966ff990c2f575846ad80b17a
CarlosArteta <arteta.carlos@gmail.com>
2024-07-18 00:19:28 +0100

    Updates tests

--- test/test_counter.py
+++ test/test_counter.py
@@ -5,21 +5,23 @@ from src.penguin_counter_model import penguinCounterNet
 import torch
 import torchvision as tv
 import scipy.io
+import scipy.ndimage 
 
 TEST_IM_PATH = (Path(__file__).parent.parent / "test/testdata/AITCb2015a_0001.JPG").absolute()
 TEST_DENSITY_PATH = (Path(__file__).parent.parent / "test/testdata/AITCb2015a_0001.mat").absolute()
 MODEL_PATH = (Path(__file__).parent.parent / "static/penguinCounterNetReduced.pth").absolute() 
 AVG_IM_PATH = (Path(__file__).parent.parent / "static/penguinCounterAvgIm.mat").absolute() 
 
-def test_penguinCounterNet():
-    im = tv.io.read_image(TEST_IM_PATH)
+def test_density_sum():
+    im = tv.io.read_image(TEST_IM_PATH).type(torch.float32)
     density = scipy.io.loadmat(TEST_DENSITY_PATH)['density']
     model = penguinCounterNet(MODEL_PATH, AVG_IM_PATH)
     model.eval()
-    with torch.no_grad():
-        pred_mask, pred_density = model(im)
-    # assert that the predicted density map is close to the reference desntiy map
-    assert np.allclose(pred_density.numpy(), density, atol=1e-2)
+    avg_im = scipy.ndimage.zoom(model.avg_image, np.array(im.size()) / model.avg_image.shape)
+    im = im - torch.tensor(avg_im)
+    with torch.inference_mode():
+        pred_mask, pred_density = model(im) 
+    assert torch.allclose(pred_density.sum(), torch.tensor(density.sum()), atol=1e-2)
     
 
     
