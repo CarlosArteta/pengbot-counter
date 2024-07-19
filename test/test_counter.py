a0bdc76101162706d303647318e3661b337b754f
CarlosArteta <arteta.carlos@gmail.com>
2024-07-19 14:34:37 +0100

    Adds gpu test

--- test/test_counter.py
+++ test/test_counter.py
@@ -12,16 +12,28 @@ TEST_DENSITY_PATH = (Path(__file__).parent.parent / "test/testdata/AITCb2015a_00
 MODEL_PATH = (Path(__file__).parent.parent / "static/penguinCounterNetReduced.pth").absolute() 
 AVG_IM_PATH = (Path(__file__).parent.parent / "static/penguinCounterAvgIm.mat").absolute() 
 
-def test_density_sum():
+def test_density_sum_cpu():
     im = tv.io.read_image(TEST_IM_PATH).type(torch.float32)
     density = scipy.io.loadmat(TEST_DENSITY_PATH)['density']
     model = penguinCounterNet(MODEL_PATH, AVG_IM_PATH)
     model.eval()
-    avg_im = scipy.ndimage.zoom(model.avg_image, np.array(im.size()) / model.avg_image.shape)
+    avg_im = scipy.ndimage.zoom(model.average_image, np.array(im.size()) / model.average_image.shape)
     im = im - torch.tensor(avg_im)
     with torch.inference_mode():
-        pred_mask, pred_density = model(im) 
-    assert torch.allclose(pred_density.sum(), torch.tensor(density.sum()), atol=1e-2)
-    
+        _, pred_density = model(im) 
+    assert torch.allclose(pred_density.sum(), torch.tensor(density.sum()), atol=1e-1)
 
+@pytest.mark.skipif(not torch.cuda.is_available(), reason="GPU not available")    
+def test_density_sum_gpu():
+    im = tv.io.read_image(TEST_IM_PATH).type(torch.float32)
+    density = scipy.io.loadmat(TEST_DENSITY_PATH)['density']
+    model = penguinCounterNet(MODEL_PATH, AVG_IM_PATH)
+    model.eval()
+    model.to('cuda')
+    avg_im = scipy.ndimage.zoom(model.average_image, np.array(im.size()) / model.average_image.shape)
+    im = im - torch.tensor(avg_im)
+    im = im.to('cuda')
+    with torch.inference_mode():
+        _, pred_density = model(im) 
+    assert torch.allclose(pred_density.cpu().sum(), torch.tensor(density.sum()), atol=1e-1)
     
